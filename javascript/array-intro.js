d2304432ab94c9b0a1897788c82e140c7b7b3531
heenaalvi2102 <heenasayyad41@gmail.com>
2021-11-17 10:58:47 +0530

    first comit

---
+++ javascript/array-intro.js
@@ -0,0 +1,119 @@
+
+
+let number = [
+    1,
+    "two",
+    {name: "codekul",tech: "MERN"},
+    4,
+    5,
+    ["a","b","c"],
+
+];
+
+let content = [
+    "codekul",
+    10,
+    20,
+    ["one","two","three"],
+    {name: "xyz", city: "pune"},
+
+];
+
+console.log(numbers);
+console.log(content);
+
+let blocks = new Array(100);
+console.log(blocks);
+
+let nums = [10,20,25,42];
+
+console.log(nums);
+
+nums.map((i)=>{
+    i*=2;
+    console.log(i);
+});
+
+let newArray = numbers.concat(content)
+
+console.log(newArray)
+
+console.log(newArray.find((element) => element < 20));
+
+console.log(newArray.indexOf(20));
+
+console.log(newArray.length);
+
+let box = new Array(500);
+
+console.log(box);
+var x = 0;
+for(i = 0;i<20;i++){
+    x+=i;
+
+    console.log(x);
+
+}
+
+let numarray = [10,20,30,40,50];
+console.log(numarray);
+numarray.splice(2,2, "sadik", "codekul");
+console.log(numarray);
+
+
+let state = (previous,current)=>{
+    console.log("State value", previous * current);
+    return previous + current;
+
+};
+
+console.log(numarray.toString())
+console.log(numarray.reduce(state));
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((response)=>{
+    //console.log(response.json())
+    let data = response.json();
+
+    console.log(data);
+    return data;
+
+   // JSON.stringify(response.json())
+
+})
+
+.then ((result) => {
+    console.log(result);
+    result.forEach((item) => {
+        var output = `
+        <div class ="card">
+        <div class ="card-body">
+        <p>${item.id}</p>
+        <p>${item.body}</p>
+
+        </div>
+        </div>
+        `;
+        document.write(output);
+        
+        
+    });
+});
+
+
+
+// axios.get("https://jsonplaceholder.typicode.com/posts")
+//         .then((response) => {
+//             console.log(response);
+
+
+
+//             data.map((item) =>{
+//                 <tr> 
+//                     <td></td>
+//                     <td></td>
+//                     <td></td>
+//                 </tr>;
+//             });
+
+//         });
---
+++ javascript/js-intro.js
@@ -0,0 +1,48 @@
+//alert("welcome to js")
+//3 keywords for declaring a variable in js
+//var,let,const
+//which of the errors in js list out.
+//var keyword variables are global scope variable as well as function scope variable
+//let keyword variables are block scope variables as well as function scope variables
+//const keyword variable for a constant value
+//creating website:-  navbar,carousel,modal,card
+//hoisting- whatever declaration part of variables as well as is there that is hoisted at the top
+
+let num1;
+num1=258
+console.log(num1)
+
+var fname="codekul"
+
+num1=35;
+
+num5=55;
+console.log(num5)
+
+let num5;
+
+console.log(fname)
+
+if(true){
+
+    console.log(num1)
+    console.log(city)
+    var num2 = 100;
+
+    console.log(num2)
+    console.log(fname)
+}
+
+function displaynum(){
+    //var fname="codekul"
+
+    let city="pune"
+    var num3 = 150;
+    console.log(num3)
+
+}
+
+displaynum()
+console.log(num2)
+
+console.log(fname)
